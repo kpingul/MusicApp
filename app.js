d502200909b8da1b040bd0d015515fbc18dc8ce1
kpingul <kirckpingul@gmail.com>
2015-06-13 18:29:05 -0700

    updated routing using UI router and $stateparams

--- app.js
+++ app.js
@@ -15,7 +15,38 @@ APP.config(['$urlRouterProvider', '$stateProvider' , function($urlRouterProvider
 			templateUrl: 'client/templates/home.html',
 
 			controller: function($scope) {
-				
+
+				$scope.data = [
+					{
+						id: '0',
+						artist: 'Jay-z',
+						title: 'December 4th',
+						rating: 5
+
+					},
+					{
+						id: '1',
+						artist: 'Kygo',
+						title: 'I see fire',
+						rating: 4
+
+					},
+					{
+						id: '2',
+						artist: 'Chris Brown',
+						title: 'War for you',
+						rating: 3
+
+					},
+				]
+
+			
+
+
+
+
+
+
 			}
 		})
 		.state('add', {
@@ -25,6 +56,19 @@ APP.config(['$urlRouterProvider', '$stateProvider' , function($urlRouterProvider
 
 			controller: function($scope) {
 				
+			}
+		})
+		.state('edit', {
+			url: '/edit/:id/:artist/:title/:rating', 
+
+			templateUrl: 'client/templates/editMusic.html',
+
+			controller: function($scope, $stateParams) {
+
+				$scope.artist = $stateParams.artist;
+				$scope.song = $stateParams.title;
+				$scope.rating = $stateParams.rating;
+				console.log($scope.rating);
 			}
 		});
 	
