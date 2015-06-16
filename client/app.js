2a28ec320e75119cb8639d48d4c41d0626a26292
kpingul <kirckpingul@gmail.com>
2015-06-15 20:41:02 -0700

    added a routes folder

--- client/app.js
+++ client/app.js
@@ -1,48 +1,6 @@
 /* Angular Application */
 
-var APP = angular.module('APP', ['ui.router']);
-
-/* Routing Config */
-
-APP.config(['$urlRouterProvider', '$stateProvider' , function($urlRouterProvider, $stateProvider){
-
-	$urlRouterProvider.otherwise('/');
-
-
-	$stateProvider
-
-		.state('home', {
-
-			url: '/',
-
-			templateUrl: 'client/templates/home.html',
-
-			controller: 'mainCtrl'
-
-		})
-
-		.state('add', {
-
-			url: '/add',
-
-			templateUrl: 'client/templates/addMusic.html',
-
-			controller: 'addCtrl'
-
-		})
-
-		.state('edit', {
-
-			/* Dynamic routing using $stateparams to catch values from data */
-			url: '/edit/:id/:artist/:title/:rating', 
-
-			templateUrl: 'client/templates/editMusic.html',
-
-			controller: 'editCtrl'
-		});
-	
-
-}]);
+var APP = angular.module('APP', []);
 
 
 
---
+++ client/routes/routeconfig.js
@@ -0,0 +1,48 @@
+/* Routing Config */
+
+(function (){
+
+		'use strict';
+
+		angular.module('APP', ['ui.router'])
+			
+			.config(['$urlRouterProvider', '$stateProvider' , function($urlRouterProvider, $stateProvider){
+
+					$urlRouterProvider.otherwise('/');
+
+
+					$stateProvider
+
+						.state('home', {
+
+							url: '/',
+
+							templateUrl: 'client/templates/home.html',
+
+							controller: 'mainCtrl'
+
+						})
+
+						.state('add', {
+
+							url: '/add',
+
+							templateUrl: 'client/templates/addMusic.html',
+
+							controller: 'addCtrl'
+
+						})
+
+						.state('edit', {
+
+							/* Dynamic routing using $stateparams to catch values from data */
+							url: '/edit/:id/:artist/:title/:rating', 
+
+							templateUrl: 'client/templates/editMusic.html',
+
+							controller: 'editCtrl'
+						});
+					
+
+				}]);
+})();
